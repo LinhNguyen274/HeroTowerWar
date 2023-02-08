0d3bc129d9094afcf3fd22ddafdb8ad785c4e728
MSI\LINH <120379173+LinhNguyen274@users.noreply.github.com>
2023-02-08 22:47:04 +0700

    optimised comment 2

--- src/settings.4b6fc.js
+++
@@ -1,1 +0,0 @@
-window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/HomeScene.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"01588",resources:"a06f7",main:"9c0cb"}};
---
+++ src/settings.f5f0e.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scenes/HomeScene.fire",orientation:"portrait",jsList:[],bundleVers:{internal:"01588",resources:"0792c",main:"4ca9e"}};
